7e78db6899b1d912c1be109d71de5f918e60859e
sathu0622 <sathushan622@gmail.com>
2025-03-06 09:58:40 +0530

    axios_upadte

--- geethanjalie_frontend/src/hook/axiosInstance.js
+++ geethanjalie_frontend/src/hook/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: ` http://localhost:5000/api`,
+    baseURL: `${process.env.REACT_APP_HOST}api`,
     headers: {
         'Content-Type': 'application/json',
     },
